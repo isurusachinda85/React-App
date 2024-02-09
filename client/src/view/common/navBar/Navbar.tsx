fd2029986ca476e822ea1fbfdf229e84e97ef687
isurusachinda85 <isurusachintha8@gmail.com>
2024-02-10 04:44:58 +0530

    update navbar

--- client/src/view/common/navBar/Navbar.tsx
+++ client/src/view/common/navBar/Navbar.tsx
@@ -5,49 +5,67 @@ import {Button} from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import IconButton from '@mui/material/IconButton';
+import Badge, {BadgeProps} from '@mui/material/Badge';
+import {styled} from '@mui/material/styles';
+import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
 
+const StyledBadge = styled(Badge)<BadgeProps>(({theme}) => ({
+    '& .MuiBadge-badge': {
+        right: -3,
+        top: 2,
+        border: `1px solid ${theme.palette.background.paper}`,
+        padding: '0 4px',
+    },
+}));
 
 export class Navbar extends Component {
     render() {
         return (
 
             <>
-                <nav className="p-5 flex justify-between">
+                {/*Nav 01*/}
+                <nav className="p-4 h-24 flex justify-between items-center">
 
-                    <div className="flex space-x-1">
+                    <div className="flex items-center h-full space-x-1">
                         <img className="w-[70px] " src={logo} alt=""/>
-                        <h1 className="mt-2.5 text-[20px] text-[#0243E9] font-bold">Techno Mobile</h1>
+                        <h1 className="text-[20px] text-[#0243E9] font-bold">Techno Mobile</h1>
                     </div>
 
-                    <div className="w-[43%] flex ">
-                        <input className="float-right w-full h-full border-[2px] border-[#2560F8] border-r-0 rounded rounded-r-none focus:outline-none"
-                               type="text"
-                               placeholder="Search items"/>
-                        <button className="rounded-r bg-[#2560F8] text-white w-56">{<SearchIcon />}</button>
-                    </div>
-
-
-                    <div>
-                        <Button className="h-full" startIcon={<AddShoppingCartIcon />}>
-                            <Link to="/mycart">my cart (0)</Link>
-                        </Button>
+                    <div className="w-[43%] h-full flex items-center">
+                        <input
+                            className="pl-2 float-right w-full h-12 border-[2px] border-[#2560F8] border-r-0 rounded rounded-r-none focus:outline-none"
+                            type="text"
+                            placeholder="Search items"/>
+                        <button className="rounded-r bg-[#2560F8] h-12 text-white w-32">{<SearchIcon/>}</button>
                     </div>
 
 
-                    <div>
-                        <Button className="h-full" startIcon={<AccountCircleIcon />}>
+                    <div className="h-full flex items-center">
+                        <IconButton aria-label="cart">
+                            <Link to="/mycart">
+                                <StyledBadge badgeContent={5} color="error">
+                                    <AddShoppingCartIcon/>
+                                </StyledBadge>
+                            </Link>
+                        </IconButton>
+                    </div>
+
+
+                    <div className="h-full flex items-center">
+                        <Button className="h-12" startIcon={<AccountCircleIcon/>}>
                             <Link to="/user">welcome user</Link>
                         </Button>
                     </div>
 
-                    <div>
-                        <Button className="h-full w-28" size="medium">
+                    <div className="h-full flex items-center">
+                        <Button className="h-12 w-28" size="medium">
                             <Link to="/login">Login</Link>
                         </Button>
                     </div>
 
-                    <div>
-                        <Button className="h-full w-28" size="medium">
+                    <div className="h-full flex items-center">
+                        <Button className="h-12 w-28" size="medium">
                             <Link to="/signup">Sign Up</Link>
                         </Button>
                     </div>
@@ -55,25 +73,38 @@ export class Navbar extends Component {
                 </nav>
 
 
+                {/*Nav 02*/}
+
                 <div className="p-4 bg-[#2560F8]  ">
-                    <ul className="list-none flex justify-center text-[#e6f0e6] text-[17px] font-bold">
-                        <li className="mr-10 hover:text-black  duration-500">
+                    <ul className="list-none flex justify-center gap-12 text-[#e6f0e6] text-[17px] font-bold">
+                        <li className="hover:text-black  duration-500">
                             <Link to="/">Home</Link>
                         </li>
 
-                        <li className="mr-10 hover:text-black duration-500">
-                            <Link to="/mobilephone">Mobile Phones</Link>
+                        <li className="group relative hover:text-black cursor-pointer duration-500 flex items-center ">
+
+                            Mobile Phones <ArrowDropDownOutlinedIcon/>
+                            <div className="absolute -left-2 top-6 z-[99999]  hidden w-[150px] bg-white p-2 group-hover:block">
+                                <ul className="space-y-3 pt-5">
+                                    <li>
+                                        <Link to="/mobilephone">Apple</Link>
+                                    </li>
+                                    <li>Samsung</li>
+                                    <li>Oppo</li>
+                                    <li>Huawei</li>
+                                </ul>
+                            </div>
                         </li>
 
-                        <li className="mr-10 hover:text-black duration-500">
+                        <li className="hover:text-black duration-500">
                             <Link to="/accessories">Accessories</Link>
                         </li>
 
-                        <li className="mr-10 hover:text-black duration-500">
+                        <li className="hover:text-black duration-500">
                             <Link to="/aboutus">About Us</Link>
                         </li>
 
-                        <li className="mr-10 hover:text-black duration-500">
+                        <li className="hover:text-black duration-500">
                             <Link to="/contactus">Contact Us</Link>
                         </li>
                     </ul>
--- client/src/view/pages/home/Home.tsx
+++ client/src/view/pages/home/Home.tsx
@@ -3,9 +3,13 @@ import {Component} from "react";
 export class Home extends Component {
     render() {
         return (
-            <>
-                <h1 className="text-5xl text-center ">Home</h1>
-            </>
+            <div>
+
+                <div className={"m-10 w-[full] h-72 border border-black"}></div>
+
+                <div className={"m-10 w-[full] h-[60px] border border-black"}></div>
+
+            </div>
         );
     }
 }
