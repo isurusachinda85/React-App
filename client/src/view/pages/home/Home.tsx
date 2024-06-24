2b87606f883cfeec949c545389639bccd521eb64
isurusachinda85 <isurusachintha8@gmail.com>
2024-06-25 04:01:26 +0530

    create image slider

--- client/src/view/pages/home/Home.tsx
+++ client/src/view/pages/home/Home.tsx
@@ -1,9 +1,6 @@
 import { Component } from "react";
-import img from "../../../image/maxresdefault.jpg"
-
 import Slider from "react-slick";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -11,11 +8,41 @@ import im1 from "../../../image/slider/im1.jpg";
 import im2 from "../../../image/slider/im2.jpg";
 import im3 from "../../../image/slider/im3.jpg";
 import im4 from "../../../image/slider/im4.jpg";
+import CommonButton from "../../common/Button/Button";
 
+interface ArrowProps {
+    className?: string;
+    style?: React.CSSProperties;
+    onClick?: () => void;
+}
+
+function SampleNextArrow(props: ArrowProps) {
+    const { className, style, onClick } = props;
+    return (
+        <button
+            className={className}
+            /* style={{ ...style, display: "none", width: "50px", height: "50px", border: "1px solid white", borderRadius: "50%", position: "absolute", right: "30px" }} */
+            style={{ ...style, display: "none" }}
+            onClick={onClick}
+        />
+    );
+}
+
+function SamplePrevArrow(props: ArrowProps) {
+    const { className, style, onClick } = props;
+    return (
+        <button
+            className={className}
+            style={{ ...style, display: "none" }}
+            onClick={onClick}
+        />
+    );
+}
 
 export class Home extends Component {
     render() {
         const settings = {
+            fade: true,
             dots: true,
             infinite: true,
             speed: 500,
@@ -23,6 +50,8 @@ export class Home extends Component {
             slidesToScroll: 1,
             autoplay: true,
             autoplaySpeed: 3000,
+            nextArrow: <SampleNextArrow />,
+            prevArrow: <SamplePrevArrow />
         };
 
         const images = [
@@ -34,18 +63,14 @@ export class Home extends Component {
         return (
             <>
 
-                <div className="border border-black h-[300vh]">
-                    {/* <div className={"w-[full] h-[500px] mr-24 ml-24 mt-10 border border-black flex"}>
-                        <img className="w-full" src={img} alt="" />
-                    </div>
-                    */}
+                <div className="h-[300vh]">
 
-                    <Box className="border-black  mt-5 w-[full] h-[500px] bg-blue-300 ">
-                        {<Slider {...settings} className="border border-black w-full ">
+                    <Box className="mt-5 w-full h-[500px]  pr-32 pl-32">
+                        {<Slider {...settings} className="w-[100%] h-[500px] ">
                             {images.map((image, index) => (
                                 <Box key={index} className="relative">
-                                    {/* <img src={image.url} alt={image.title} className="w-full" /> */}
-
+                                    <img src={image.url} alt={image.title} className="w-full h-[500px]" />
+                                    <CommonButton className="absolute left-28 bottom-24" variant="contained" label="Shop now" style={{ background: "#09bafa" }} />
                                 </Box>
                             ))}
                         </Slider>}
