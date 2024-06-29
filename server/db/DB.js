70b848e722863fae809746d922ef56c8eb25bae6
isurusachinda85 <isurusachintha8@gmail.com>
2024-06-29 14:12:28 +0530

    update server

---
+++ server/db/DB.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+const db = async () => {
+  try {
+    const conn = await mongoose.connect(MONGODB_URL);
+    console.info(`MongoDB Connected to : ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB Connect Error : ", error);
+  }
+};
+module.exports.db();
